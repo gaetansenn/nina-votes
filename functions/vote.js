bc3dea10023e9ddd011e0f3437192eea5d99df3c
Gaetan SENN <gaetan.senn@gmail.com>
2020-09-01 17:51:51 +0200

    functions: clean up

--- functions/vote.js
+++ functions/vote.js
@@ -1,5 +1,4 @@
 exports.handler = async function (event, context, callback) {
-  console.log(`{\ncontext: ${JSON.stringify(context,null,2)},\nevent: ${JSON.stringify(event,null,2)}\n}`)
   callback(null, {
     statusCode: 200,
     body: `Your IP address ${event.headers['client-ip']}`
